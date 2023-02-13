a901e50e1a015d52a8617b6a8f4be04895d5ad80
Deech-Teach <76897436+Deech-Teach@users.noreply.github.com>
2023-02-13 12:50:12 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,10 +3,10 @@ var backgroundImage, car1_img, car2_img, track;
 var fuelImage, powerCoinImage, lifeImage;
 var obstacle1Image, obstacle2Image;
 var database, gameState;
+var blastImage;
 var form, player, playerCount;
 var allPlayers, car1, car2, fuels, powerCoins, obstacles;
 var cars = [];
-var blastImage;
 
 function preload() {
   backgroundImage = loadImage("./assets/background.png");
